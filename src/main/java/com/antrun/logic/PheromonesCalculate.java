eea79eec2a23d62e0203b9693164fdeeab190e22
Rattanakanahutanon, Sittikun (Agoda) <sittikun.rattanakanahutanon@agoda.com>
2016-05-28 22:40:14 +0700

    Add pheromones Update

--- src/main/java/com/antrun/logic/PheromonesCalculate.java
+++ src/main/java/com/antrun/logic/PheromonesCalculate.java
@@ -8,17 +8,11 @@ import java.util.List;
  */
 public final class PheromonesCalculate {
 	class PairIndex{
-		int i,j;
+		public final int i,j;
 		public PairIndex(final int i,final int j){
 			this.i = i;
 			this.j = j;
 		}
-        public int getI() {
-            return i;
-        }
-        public int getJ() {
-            return j;
-        }
     }
 	
     /***
@@ -61,25 +55,21 @@ public final class PheromonesCalculate {
     public double[][] pheromonesUpdateCalculate(final double[][] pheromonesArray, List<Integer> listOfTown, double bestDistanceOfLastRun){
         double[][] result = pheromonesArray.clone();
         double extraPheromonesFromLastRun = 1d/bestDistanceOfLastRun;
+        List<PairIndex> pairIndexList = genneratePairIndex(listOfTown);
 
-        for(int i = 0 ; i < pheromonesArray.length ; i++){
-            for(int j = 0 ; j < pheromonesArray[i].length ; j++){
-                if(i==j)
-                    result[i][j] = pheromonesArray[i][j];
-                else
-                    result[i][j] = pheromonesArray[i][j] + extraPheromonesFromLastRun;
-            }
+        for(PairIndex p: pairIndexList){
+                    result[p.i][p.j] = pheromonesArray[p.i][p.j] + extraPheromonesFromLastRun;
         }
         return result;
     }
     
-    public List<PairIndex> gennerate(List<Integer> listOfTown){
+    public List<PairIndex> genneratePairIndex(List<Integer> listOfTown){
     	List<PairIndex> result = new  ArrayList<PairIndex>();
         if(listOfTown.size() < 2){
             return result;
         }else{
             for(int i = 1; i < listOfTown.size() ; i++){
-                result.add(new PairIndex(listOfTown.get(i), listOfTown.get(i+1)));
+                result.add(new PairIndex(listOfTown.get(i-1), listOfTown.get(i)));
             }
             return result;
         }
--- src/test/java/test/com/antrun/logic/PheromonesCalculateTest.java
+++ src/test/java/test/com/antrun/logic/PheromonesCalculateTest.java
@@ -3,6 +3,11 @@ package test.com.antrun.logic;
 import com.antrun.logic.PheromonesCalculate;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.Assert.assertArrayEquals;
 
 /**
@@ -36,5 +41,24 @@ public class PheromonesCalculateTest {
         };
 
         assertArrayEquals(expected, p.pheromonesVolatileCalculate(p.initPheromones(5), 0.02d));
+    }
+
+    @Test
+    public void pheromonesUpdateCalculateTest(){
+        PheromonesCalculate p = new PheromonesCalculate();
+        p.initPheromones(5);
+        List<Integer> hu_towns = new ArrayList(Arrays.asList(3,4,2,5,1));
+        List<Integer> towns = hu_towns.stream().map(x-> x-1).collect(Collectors.toList());
+        double[][] list_0_98 = p.pheromonesVolatileCalculate(p.initPheromones(5), 0.02d);
+
+        double[][] expected = {
+                {0,0.98,0.98,0.98,0.98},
+                {0.98,0,0.98,0.98,1.012258064516129},
+                {0.98,0.98,0,1.012258064516129,0.98},
+                {0.98,1.012258064516129,0.98,0,0.98},
+                {1.012258064516129,0.98,0.98,0.98,0}
+        };
+
+        assertArrayEquals(expected, p.pheromonesUpdateCalculate(list_0_98, towns, 31));
     }
 }
